b0369aa9f16ef8f9aa2779a0985713cdaa09749e
​dino-21 <ysboo2@naver.com>
2024-03-28 18:49:58 +0900

    댓글 이벤트처리_html처리

---
+++ src/main/java/org/zerock/service/ReplyServiceImpl.java
@@ -0,0 +1,58 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyPageDTO;
+import org.zerock.domain.ReplyVO;
+import org.zerock.mapper.ReplyMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+@AllArgsConstructor
+public class ReplyServiceImpl implements ReplyService{
+	
+	private ReplyMapper mapper;
+	
+	@Override
+	public int register(ReplyVO vo) {
+		log.info("register......" + vo);
+		return mapper.insert(vo);
+	}
+
+	@Override
+	public ReplyVO get(Long rno) {
+		log.info("get......" + rno);
+		return mapper.read(rno);
+	}
+
+	@Override
+	public int modify(ReplyVO vo) {
+		log.info("modify......" + vo);
+		return mapper.update(vo);
+	}
+
+	@Override
+	public int remove(Long rno) {
+		log.info("remove...." + rno);
+		return mapper.delete(rno);
+	}
+
+	@Override
+	public List<ReplyVO> getList(Criteria cri, Long bno) {
+		log.info("get Reply List of a Board " + bno);
+		return mapper.getListWithPaging(cri, bno);
+	}
+
+	// 댓글의 총 개수와 특정 페이지의 댓글 목록을 담아 반환
+	// 게시글에 대한 전체 댓글 수,  특정 페이지의 댓글 목록(list)을 담아 반환
+	@Override
+	public ReplyPageDTO getListPage(Criteria cri, Long bno) {
+		return new ReplyPageDTO(mapper.getCountByBno(bno), mapper.getListWithPaging(cri, bno));
+	}
+
+}
